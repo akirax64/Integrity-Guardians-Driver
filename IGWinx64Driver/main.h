085921ab82037b4c6df4b251b4a0fcb529f825d6
leankk <leandrofamiliafox@gmail.com>
2025-08-24 21:41:08 -0300

    refactor: refactoring the main, organizing it and leaving only what is necessary for the safe loading and unloading of the driver

--- IGWinx64Driver/main.h
+++ IGWinx64Driver/main.h
@@ -6,14 +6,7 @@
 
 #pragma once
 
-// prototipos
-
 NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT d_Object, _In_ PUNICODE_STRING r_Path);
-NTSTATUS Unload(_In_ FLT_FILTER_UNLOAD_FLAGS flags);
-NTSTATUS DeviceControl(_In_ PDEVICE_OBJECT deviceObject, _Inout_ PIRP irp);
-NTSTATUS ConnectionNotifyCallback(_In_ PFLT_PORT clientPort, _In_ PVOID serverPortCookie, _In_ PVOID connectionContext, _In_ ULONG size, _Out_ PVOID* connectionPortCookie);
-VOID DisconnectionNotifyCallback(_In_ PVOID connectionCookie);
-NTSTATUS MessageNotifyCallback(_In_ PVOID portCookie, _In_ PVOID inputBuffer, _In_ ULONG inputBufferLength, _Out_ PVOID outputBuffer, _In_ ULONG outputBufferLength, _Out_ PULONG returnOutputBufferLength);
-
+VOID DriverUnload(_In_ PDRIVER_OBJECT driverObject);
 #endif // !MAIN_H
 
